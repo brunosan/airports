92bb9ee8879f32504c23837450fd0404c848ca70
Bruno Sanchez-Andrade Nuño <brunosan@gmail.com>
2019-02-14 14:29:43 +0100

    UI details

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ $(function() {
     console.log("countries loaded");//,countries);
 
     console.log("reading airports...");
+    $("#origin").val("Loading Airports...")
+    $("#destination").val("Loading Airports...")
     //TODO Use https://stuk.github.io/jszip/documentation/howto/read_zip.html
     $.get(airports_file, function(data)
       {
@@ -64,9 +66,12 @@ $(function() {
 });
 });
 
+$( "#submit" ).click(function() {
+  alert( "This button would start the process to issue the permits." );
+});
+
 function mapit(){
   console.log("Setting up map..");
-
 
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
@@ -79,13 +84,15 @@ function mapit(){
 function calculate_overpass(origin_ican,destination_ican,airports){
     //Clean any previous marker and layers
     for ( layer in extra_layers ){
-      console.log(extra_layers[layer]);
       extra_layers[layer].remove();
     }
 
     //Set origin and destination
     origin = airports[origin_ican]
     destination = airports[destination_ican]
+    $("#origin").val(origin.name)
+    $("#destination").val(destination.name)
+    $("#permits").val("")
     console.log(origin_ican,destination_ican,origin,destination);
 
     //add markers and straight route to the map.
@@ -97,7 +104,7 @@ function calculate_overpass(origin_ican,destination_ican,airports){
 
     var start = turf.point([origin.lon, origin.lat]);
     var end = turf.point([destination.lon, destination.lat,]);
-    var greatCircle = turf.greatCircle(start, end, {'name': 'Seattle to DC'});
+    var greatCircle = turf.greatCircle(start, end);
 
     var greatCircleMap = new L.geoJSON(greatCircle, {
     }).addTo(mymap);
@@ -111,6 +118,7 @@ function calculate_overpass(origin_ican,destination_ican,airports){
         if (intersection.features.length!=0) {
           var country = new L.geoJSON(feature).addTo(mymap).bindPopup(feature.properties.name);
           extra_layers.push(country)
+          $("#permits").val($("#permits").val()+"Crosses: "+feature.id+" "+feature.properties.name+"\n")
           console.log("Crosses: ",feature.id," ",feature.properties.name);
         }
       });
