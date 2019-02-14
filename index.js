b2467ef957d4a36a8722f6e215e9edb817d3ff63
Bruno Sanchez-Andrade Nuño <brunosan@gmail.com>
2019-02-14 14:12:09 +0100

    attach autocomplete, clean

--- index.js
+++ index.js
@@ -1,77 +1,62 @@
 var cluster = [];
-var airports = [];
+var airports = {};
 var countries = [];
 var countries_file = 'countries.json';
 var airports_file = 'airports.json';
 var mymap = L.map('map').setView([0.505, -0.09], 3);
-
+var origin_ican = "FEGG"
+var destination_ican = "DTTA"
+var extra_layers = []
 mapit();
 
+function getAirportByName(object, value) {
+  return Object.keys(object).find(key => object[key].name === value);
+}
+
 $(function() {
-
-  var point = turf.point([-98.768159, 20.098287]); // Hgo | This should return one TRUE at least
-  // var point = turf.point([-79.601038, 43.6565353]); // Toronto | This should always be FALSE
-
-
   console.log("reading countries...");
   $.getJSON(countries_file, function(data)
   {
     countries = data;
     console.log("countries loaded");//,countries);
 
-
     console.log("reading airports...");
     //TODO Use https://stuk.github.io/jszip/documentation/howto/read_zip.html
     $.get(airports_file, function(data)
       {
         airports = JSON.parse(data);
+        airportnames=[]
+        for (var key in airports){
+          airportnames.push(airports[key].name);
+        }
         console.log("airports loaded");//,Object.keys(airports));
-
-
-        //Set origin and destination
-        origin_ican = "FAYP"
-        destination_ican = "HESH"
-        origin = airports[origin_ican]
-        destination = airports[destination_ican]
-
-        //add markers and straight route to the map.
-        L.marker([origin.lat, origin.lon]).addTo(mymap)
-      		.bindPopup(origin.name);
-        L.marker([destination.lat, destination.lon]).addTo(mymap)
-        	.bindPopup(destination.name);
-
-
-        var start = turf.point([origin.lon, origin.lat]);
-        var end = turf.point([destination.lon, destination.lat,]);
-        var greatCircle = turf.greatCircle(start, end, {'name': 'Seattle to DC'});
-
-        L.geoJSON(greatCircle, {
-        }).addTo(mymap);
-
-
-
-        console.log("Computing intersection between:",origin.name," and ",destination.name);
-
-        //See which countries intersect
-
-        countries.features.forEach(function(feature) {
-
-            var intersection = turf.lineIntersect(greatCircle, feature);
-            if (intersection.features.length!=0) {
-              L.geoJSON(feature).addTo(mymap);
-              console.log("Crosses: ",feature.id," ",feature.properties.name);
-
+        calculate_overpass(origin_ican,destination_ican,airports);
+        //Set up autcomplete fields
+        $( function() {
+          var availableTags = airportnames;
+          $( "#origin" ).autocomplete({
+            source: availableTags,
+            minChars: 3,
+            select: function (event, ui) {
+              origin_ican = getAirportByName(airports,ui.item.value)
+              console.log(ui.item.value,origin_ican);
+              if (destination_ican!=""){
+                calculate_overpass(origin_ican,destination_ican,airports);
+              }
             }
           });
-
-        //Zoom in
-        mymap.fitBounds([
-          [origin.lat, origin.lon],
-          [destination.lat, destination.lon]
-        ]);
-
-
-
+          $( "#destination" ).autocomplete({
+            source: availableTags,
+            minChars: 3,
+            select: function (event, ui) {
+              destination_ican = getAirportByName(airports,ui.item.value)
+              console.log(ui.item.value,destination_ican);
+              if (origin_ican!=""){
+                calculate_overpass(origin_ican,destination_ican,airports);
+              }
+            }
+          });
+        } );
 
   }, 'text');
 
@@ -89,7 +74,50 @@ function mapit(){
 			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 		id: 'mapbox.streets'
 	}).addTo(mymap);
+};
+
+function calculate_overpass(origin_ican,destination_ican,airports){
+    //Clean any previous marker and layers
+    for ( layer in extra_layers ){
+      console.log(extra_layers[layer]);
+      extra_layers[layer].remove();
+    }
+
+    //Set origin and destination
+    origin = airports[origin_ican]
+    destination = airports[destination_ican]
+    console.log(origin_ican,destination_ican,origin,destination);
+
+    //add markers and straight route to the map.
+    var marker_origin      = new L.marker([origin.lat     , origin.lon     ]).addTo(mymap).bindPopup(origin.name);
+    extra_layers.push(marker_origin);
+    var marker_destination = new L.marker([destination.lat, destination.lon]).addTo(mymap).bindPopup(destination.name);
+    extra_layers.push(marker_destination);
 
 
+    var start = turf.point([origin.lon, origin.lat]);
+    var end = turf.point([destination.lon, destination.lat,]);
+    var greatCircle = turf.greatCircle(start, end, {'name': 'Seattle to DC'});
 
-}
+    var greatCircleMap = new L.geoJSON(greatCircle, {
+    }).addTo(mymap);
+    extra_layers.push( greatCircleMap );
+
+    console.log("Computing intersection between:",origin.name," and ",destination.name);
+
+    //See which countries intersect
+    countries.features.forEach(function(feature) {
+        var intersection = turf.lineIntersect(greatCircle, feature);
+        if (intersection.features.length!=0) {
+          var country = new L.geoJSON(feature).addTo(mymap).bindPopup(feature.properties.name);
+          extra_layers.push(country)
+          console.log("Crosses: ",feature.id," ",feature.properties.name);
+        }
+      });
+
+    //Zoom in
+    mymap.fitBounds([
+      [origin.lat, origin.lon],
+      [destination.lat, destination.lon]
+    ]);
+};
